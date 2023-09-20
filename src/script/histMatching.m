a86525db0b1fddf5d60779b20ab198559d844c93
Muhammad Fahmi Irfan <fahmiirfan1618@gmail.com>
2023-09-20 11:37:25 +0700

    add histMatching tp blm di test

---
+++ src/script/histMatching.m
@@ -0,0 +1,27 @@
+function histMatching = histMatching (input, reference)
+    sz_input = size(input);
+    sz_ref = size(reference);
+    if(sz_input == sz_ref)
+        [x,y,colors] = size(input);
+        input_histeq = histEqualizer(input);
+        ref_histeq = histEqualizer(reference);
+
+        for c=1:colors
+            inv_hist = zeros(256);
+            for i=1:256
+                minval = abs(input_histeq(i) - ref_histeq(0));
+                for j=1:256
+                    if(abs(input_histeq(i) - ref_histeq(i)) < minval)
+                        minval = abs(input_histeq(i) - ref_histeq(i));
+                        inv_hist(i) = j;
+                    end
+                end
+            end
+            input(:,:,c) = inv_hist(input(:,:,c));
+        end
+
+        histMatching = input
+    else
+        disp("Ukuran gambar input dan gambar acuan berbeda.")
+    end
+end 
